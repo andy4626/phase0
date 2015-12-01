8015ad527b9420acef579db05ebc1e5209b8847f
Andrew <andrew.kdy@gmail.com>
2015-12-01 01:07:19 -0500

    do math problems

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,21 @@
+def add (a,b)
+	puts a+b
+end
+
+def subtract(a,b)
+	puts a-b
+end
+
+def multiply(a,b)
+	puts a*b
+end
+
+def divide (a,b)
+	answer= a/b
+	puts answer.to_f
+end
+
+	add(10,2)
+	divide(10,2)
+	subtract(10,2)
+	multiply(10,2)
