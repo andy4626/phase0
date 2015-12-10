ba3fa416e48e0d0b35551199cf7f5b0a8e7eacb2
Andrew <andrew.kdy@gmail.com>
2015-12-09 20:14:46 -0500

    group accountability exercise

---
+++ week-5/group-research-methods/my_solution.rb
@@ -0,0 +1,93 @@
+# Research Methods
+
+# I spent [] hours on this challenge.
+
+i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
+my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
+
+# Person 1's solution
+def my_array_finding_method(source, thing_to_find)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_finding_method(source, thing_to_find)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+# Person 2
+def my_array_modification_method!(source, thing_to_modify)
+  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_modification_method!(source, thing_to_modify)
+  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Person 3
+def my_array_sorting_method(source)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_sorting_method(source)
+   source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Person 4
+def my_array_deletion_method(array, letter)
+  array.delete_if {|x| x.to_s.include?(letter) }  
+end
+
+def my_array_deletion_method(array, letter)
+  array.delete_if {|x| x.to_s==letter) }  
+end
+# Identify and describe the Ruby method(s) you implemented.
+#I used a delete if method which deletes the selected item when the asking requirement is fulfilled.
+#I also used to_s to change array element into a string and compare if the letter is there for me to delete
+#Lastly, I used include? method to check if the selected element is containing a desired character
+
+
+# Person 5
+def my_array_splitting_method(source)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+def my_hash_splitting_method(source, age)
+  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+end
+
+# Identify and describe the Ruby method(s) you implemented.
+#
+#
+#
+
+
+# Release 1: Identify and describe the Ruby method you implemented. Teach your
+# accountability group how to use the methods.
+#
+#
+#
+
+
+# Release 3: Reflect!
+# What did you learn about researching and explaining your research to others?
+#
+#
+#
+#
